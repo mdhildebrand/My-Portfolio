7dc0c87cb7e645747f1ade737c58ecef9ca14a0a
Matt Hildebrand <hildebrand.matt@gmail.com>
2021-06-24 17:36:54 -0700

    Landing page text updates

--- src/data/about.js
+++ src/data/about.js
@@ -1,11 +1,12 @@
 export const about = [
     {
-        welcome: "Hi, my name's Matt",
-        intro: "I'm a web developer based in Vancouver, BC.\nI love cats, videogames, and web design.\nCome take a look at some of my work!",
+        welcome: "Hi, my name is Matt",
+        intro: "I'm a font-end web developer focused on user-oriented experiences and clear, coherent design.\nCome take a look at some of my work!",
+        /*Dedicated to finding creative solutions to technical problems.*/
         aboutHeader: "About Me",
-        aboutText: "I am a Developer that recently completed BCIT's Technical Web Design program. Web design and development inspires me, to find creative solutions to technical problems, to dream up enjoyable and aesthetic designs for the user to experience, and to always keep learning.\nI spent the last nine years working as a structural drafter, before deciding it was time for a change. That time gave me a lot of experience working on collaborative projects, and striving to create high-quality, detailed work on tight deadlines.\nAs a web developer, I appreciate the creative problem solving that comes with designing something aesthetic and functional. I enjoy creating dynamic websites, and am always excited for a challenge.",
+        aboutText: "I'm a front-end web developer who apreciates the creative problem solving that comes with designing something beautiful, functional, and user-focused. The plentitude of new things to learn, and new skills to develop, satisfies my drive to always keep learning.\nMy work as a structural drafter taught me a lot about working on collaborative projects, and striving to create high-quality, detailed work on tight deadlines. I valued that time, but found a spark of passion with web design that led me to BCIT's Technical Web Design program. In my time there, I gained experience with a variety of web development tools and languages. I also discovered how much I love the creative problem solving that comes with designing something beautiful and functional.\nWhen I'm not coding, you'll find me searching out new cafes, planning my next trip, or working on any number of creative projects - lately I've been constructing miniature buildings, and learning to model 3D objects with voxels.",
         skillsHeader: "Skills",
-        codeSkills: ["HTML", "SASS", "JavaScript", "PHP", "React"],
+        codeSkills: ["HTML/CSS", "SASS", "JavaScript", "PHP", "React", "WordPress"],
         desSkills: ["Photoshop", "Illustrator", "Adobe XD", "Indesign", "Github"],
         contactHeader: "Connect",
         contactText: "If you have any questions, or just want to reach out, please contact me! You can get in touch via email, or you can also check me out on my various profiles below."
